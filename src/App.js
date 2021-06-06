833558bdd562e167abb96065ff84299b105c9b8c
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-06-06 10:22:48 -0600

    objetos renderizado index

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+//import React , { Fragment } from "react"; el importar el fragment y ponerlo en la etiqueta
+//es opcional si lo dejamos vacío lo detecta como fragment
+const FirstApp = () => {
+  const saludo = "hola mundo";
+  const human = {
+    nombre: "juan",
+    edad: 30,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{saludo}</h1>
+      <p>Buenas a todos</p>
+      <pre>{JSON.stringify(human,null,3)}</pre>
+    </>
   );
-}
+};
 
-export default App;
+export default FirstApp;
