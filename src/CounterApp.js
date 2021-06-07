c23c2dd0cbfb7caa798ac7571360c0d68adc7160
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-06-07 08:45:46 -0600

    Counter app

---
+++ src/CounterApp.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+
+const CounterApp = ({value=0}) => {
+    
+    const [counter,setCounter] =useState(value);
+    
+    const handleAdd = () => {
+        setCounter(counter+1);
+        //setCounter((c)=>c+1)
+    };
+
+    const handleMinus = () => {
+        setCounter(counter-1);
+    };
+
+    const handleReset = () => {
+        setCounter(value);
+    };
+
+  return (
+    <>
+      <h1>CounterApp</h1>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleMinus}>-1</button>
+    </>
+  );
+};
+
+CounterApp.propTypes = {
+  value: PropTypes.number,
+};
+/**
+ * en esta linea se agregan las propiedades por defecto
+ */
+ CounterApp.defaultProps = {
+
+};
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import FirstApp from "./App";
-
+import CounterApp from "./CounterApp";
+/* import FirstApp from "./App"; */
 import './index.css'
 
  const $divRoot = document.getElementById("root");
 
-ReactDOM.render(<FirstApp gretings="Buenas dias" persona='Alfonso' />, $divRoot);
+ReactDOM.render(<CounterApp value={1} />, $divRoot);
