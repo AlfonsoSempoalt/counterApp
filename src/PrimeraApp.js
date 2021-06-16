f5e173b2ab8c467a511c6e1293a78fc40dfbe2a0
AlfonsoSempoalt <alfonsosempoalt@hotmail.com>
2021-06-15 19:35:45 -0600

    checking the props of FirstApp component

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 const PrimeraApp = ({saludo,subtitulo}) => {
   return (
       <>
-      <h1>{saludo}</h1>
+      <h1>{saludo} c:</h1>
       <h2>{subtitulo}</h2>
       </>
   )
--- src/tests/base/testPrimeraApp.test.js
+++ src/tests/base/testPrimeraApp.test.js
@@ -1,12 +1,27 @@
 import PrimeraApp from "../../PrimeraApp";
-import React from "react"
-import {render} from '@testing-library/react'
+import React from "react";
+import { shallow } from "enzyme";
+//import {render} from '@testing-library/react'
 
-describe('test to PrimeraApp', () => {
-    test('should show hola', () => {
+describe("test to PrimeraApp", () => {
+  /*  test('should show hola', () => {
         const saludo='hola';
         const wrapper= render(<PrimeraApp saludo={saludo}/ >)
         expect(wrapper.getByText("hola")).toBeInTheDocument();
-    })
-    
+    }) */
+
+  test("<PrimeraApp/ > should renderize properly", () => {
+    const saludo = "hoola buenas noches";
+    const wrapper = shallow(<PrimeraApp saludo={saludo} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  test("<PrimeraApp/ > test props", () => {
+    const saludo = "hoola buenas noches";
+    const subtitulo = "soy un subtitulo";
+    const wrapper = shallow(
+      <PrimeraApp saludo={saludo} subtitulo={subtitulo} />
+    );
+    const textAppSubtitles = wrapper.find("h2").text();
+    expect(textAppSubtitles).toBe(subtitulo);
+  });
 });
